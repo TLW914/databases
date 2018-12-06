bc3690739be91c2fe4934faf7f08d77601c47c63
TLW914 <leggettwatson.tyler@gmail.com>
2018-12-06 12:27:50 -0800

    created db connection

--- server/db/index.js
+++ server/db/index.js
@@ -1,5 +1,23 @@
 var mysql = require('mysql');
 
+var dbConnection = mysql.createConnection({
+  host: 'localhost',
+  user: 'student',
+  password: 'student'
+});
+
+dbConnection.connect(function(err) {
+  if (err) {
+    throw err;
+  } 
+  console.log('Connected!');
+});
+
+//dbConnection.connect();
+module.exports = dbConnection;
+
+
+
 // Create a database connection and export it from this file.
 // You will need to connect with the user "root", no password,
 // and to the database "chat".
