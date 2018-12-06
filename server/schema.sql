977030f2c2ac6c0f8658b191aa45c08a92ce20a9
TLW914 <leggettwatson.tyler@gmail.com>
2018-12-06 11:36:01 -0800

    Added schema for mysql database

--- server/schema.sql
+++ server/schema.sql
@@ -2,8 +2,24 @@ CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
+CREATE TABLE users (
+  ID INT AUTO_INCREMENT PRIMARY KEY,
+  Name text
   /* Describe your table here.*/
+);
+
+CREATE TABLE messages (
+  ID INT AUTO_INCREMENT PRIMARY KEY,
+  Message text, 
+  UserID INT,
+  FOREIGN KEY (UserID) REFERENCES users(ID)
+);
+
+CREATE TABLE rooms (
+  ID INT AUTO_INCREMENT PRIMARY KEY, 
+  Roomname text,
+  MessageID INT,
+  FOREIGN KEY (MessageID) REFERENCES messages(ID) 
 );
 
 /* Create other tables and define schemas for them here! */
@@ -12,6 +28,6 @@ CREATE TABLE messages (
 
 
 /*  Execute this file from the command line by typing:
- *    mysql -u root < server/schema.sql
+ *    mysql -u student < server/schema.sql
  *  to create the database and the tables.*/
 
