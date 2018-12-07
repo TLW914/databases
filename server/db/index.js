662289b0d533c2e11366cce911b61580a091118d
TLW914 <leggettwatson.tyler@gmail.com>
2018-12-06 16:23:13 -0800

    implemented post controller and post model workflow. created schema and tables. successfully linked database.

--- server/db/index.js
+++ server/db/index.js
@@ -1,9 +1,10 @@
 var mysql = require('mysql');
 
 var dbConnection = mysql.createConnection({
-  host: 'localhost',
+  host: 'localhost', //investigate
   user: 'student',
-  password: 'student'
+  password: 'student',
+  database: 'chat'
 });
 
 dbConnection.connect(function(err) {
@@ -21,5 +22,7 @@ module.exports = dbConnection;
 // Create a database connection and export it from this file.
 // You will need to connect with the user "root", no password,
 // and to the database "chat".
+
+
 
 
